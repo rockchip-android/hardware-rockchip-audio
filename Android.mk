22f4d21023b189a47072f7f617cf7cf2493011f5
陈辉 <chenhui@rock-chips.com>
2012-03-26 14:25:33 +0800

    for rk30sdk

--- Android.mk
+++ Android.mk
@@ -17,7 +17,7 @@ ifeq ($(BOARD_HAVE_BLUETOOTH),true)
   LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
-LOCAL_MODULE := audio.primary.rk29sdk
+LOCAL_MODULE := audio.primary.rk30sdk
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_LIBRARIES := libmedia_helper
@@ -40,7 +40,7 @@ ifeq ($(BOARD_HAVE_BLUETOOTH),true)
 endif
 
 LOCAL_STATIC_LIBRARIES := libmedia_helper
-LOCAL_MODULE := libaudiopolicy_rk29sdk
+LOCAL_MODULE := libaudiopolicy_rk30sdk
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
@@ -61,9 +61,9 @@ LOCAL_STATIC_LIBRARIES := \
     libmedia_helper
 
 LOCAL_WHOLE_STATIC_LIBRARIES := \
-    libaudiopolicy_rk29sdk
+    libaudiopolicy_rk30sdk
 
-LOCAL_MODULE := audio_policy.rk29sdk
+LOCAL_MODULE := audio_policy.rk30sdk
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
